1cf7e52dc70074032b54dd1600117b2a43072378
DragonGzee <85408746+vuneral@users.noreply.github.com>
2021-11-07 04:31:24 +0800

    Update set-br.sh

--- back/set-br.sh
+++ back/set-br.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /usr/bin
-wget -O limit-speed "https://raw.githubusercontent.com/Gl33ch3r/autoscript/main/limit-speed.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/vuneral/eco/main/limit-speed.sh"
 chmod +x limit-speed
 cd
 rm -f /root/set-br.sh
